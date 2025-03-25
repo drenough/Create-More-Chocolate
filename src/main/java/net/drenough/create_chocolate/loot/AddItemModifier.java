d060ebfc011bff97330bdb8aef6e0e74a9395fb4
drenough <dendy412@gmail.com>
2025-03-25 15:30:24 +0100

    Fully backported all new stuff and features from 1.21.1

--- src/main/java/net/drenough/create_chocolate/loot/AddItemModifier.java
+++ src/main/java/net/drenough/create_chocolate/loot/AddItemModifier.java
@@ -1,23 +1,24 @@
 package net.drenough.create_chocolate.loot;
 
+import com.google.common.base.Suppliers;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 
-public class AddItemModifier extends LootModifier {
-    public static final Codec<AddItemModifier> CODEC = RecordCodecBuilder.create(inst ->
-            LootModifier.codecStart(inst).and(
-                            BuiltInRegistries.ITEM.byNameCodec().fieldOf("item").forGetter(e -> e.item))
-                    .apply(inst, AddItemModifier::new));
+import java.util.function.Supplier;
 
+public class AddItemModifier extends LootModifier {
+    public static final Supplier<Codec<AddItemModifier>> CODEC = Suppliers.memoize(()
+            -> RecordCodecBuilder.create(inst -> codecStart(inst).and(ForgeRegistries.ITEMS.getCodec()
+            .fieldOf("item").forGetter(m -> m.item)).apply(inst, AddItemModifier::new)));
     private final Item item;
 
     public AddItemModifier(LootItemCondition[] conditionsIn, Item item) {
@@ -26,18 +27,20 @@ public class AddItemModifier extends LootModifier {
     }
 
     @Override
-    protected @NotNull ObjectArrayList<ItemStack> doApply(@NotNull ObjectArrayList<ItemStack> generatedLoot, @NotNull LootContext lootContext) {
-        for (LootItemCondition condition : this.conditions) {
-            if(!condition.test(lootContext)) {
+    protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
+        for(LootItemCondition condition : this.conditions) {
+            if(!condition.test(context)) {
                 return generatedLoot;
             }
         }
+
         generatedLoot.add(new ItemStack(this.item));
+
         return generatedLoot;
     }
 
     @Override
     public Codec<? extends IGlobalLootModifier> codec() {
-        return CODEC;
+        return CODEC.get();
     }
 }
